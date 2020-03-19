c8304d593de71dbd157bb0e538d2d035decf3f52
Никита Аврелин <nikita.avrelin@scorpion.co>
2020-03-19 10:24:37 +0100

    sequelize + simple authentication

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,9 +1,22 @@
-import { GraphQLResolverMap } from 'apollo-graphql';
-import data from './data';
+import { GraphQLResolverMap } from "apollo-graphql";
+import data from "./data";
 
 export interface IUser {
   id: string;
   name?: string;
+}
+
+export interface INewUser {
+  id?: string;
+  name: string;
+  email: string;
+  password: string;
+  token?: string;
+}
+
+export interface ISingInInput {
+  email: string;
+  password: string;
 }
 
 export interface IPost {
@@ -34,6 +47,15 @@ const resolverMap = {
     user: (id: string): IUser | undefined => {
       return data.users.find((user: IUser) => user.id === id);
     },
+    // {
+    //   users {
+    //     name
+    //   }
+    // }
+    users(_: any, __: any, { models }: { models: any }): INewUser[] {
+      console.log(models.User);
+      return models.User.findAll();
+    },
     posts: (): IPost[] => {
       return data.posts;
     },
@@ -41,31 +63,69 @@ const resolverMap = {
       return data.reviews;
     },
     post: (_: any, { id }: { id: string }): IPost | undefined => {
-      console.log('>>> id: ', id);
+      console.log(">>> id: ", id);
       return data.posts.find((post: IPost) => post.id === id);
-    },
+    }
   },
   Mutation: {
+    // mutation addUser {
+    //   addUser(user: {email: "jane@example.com", name: "Nick", password: "111"}){
+    //     name
+    //     email
+    //     password
+    //   }
+    // }
+    signInUser: async (
+      _: any,
+      { signInInput }: { signInInput: ISingInInput },
+      { models }: { models: any }
+    ): Promise<INewUser> => {
+      const { email, password } = signInInput;
+      await models.User.update(
+        { token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9" },
+        {
+          where: {
+            email,
+            password
+          },
+          returning: true
+        }
+      );
+      const user = await models.User.findOne({ where: { email } });
+      return user.dataValues;
+    },
+    addUser: async (
+      _: any,
+      { user }: { user: INewUser },
+      { models }: { models: any }
+    ): Promise<INewUser> => {
+      await models.User.create(user);
+      return user;
+    },
     addReview: (_: any, { review }: { review: IReview }): IUser => {
-      console.log('>>> addReview: ', review);
+      console.log(">>> addReview: ", review);
       data.reviews.push(review);
       return data.users[0];
-    },
+    }
   },
   User: {
     posts: (parent: IUser): IPost[] => {
       return data.posts.filter((post: IPost) => post.userId === parent.id);
     },
     reviews: (parent: IUser): IReview[] => {
-      return data.reviews.filter((review: IReview) => review.userId === parent.id);
-    },
+      return data.reviews.filter(
+        (review: IReview) => review.userId === parent.id
+      );
+    }
   },
   Post: {
     user: (parent: IPost): IUser | undefined => {
       return data.users.find((user: IUser) => user.id === parent.userId);
     },
     reviews: (parent: IPost): IReview[] => {
-      return data.reviews.filter((review: IReview) => review.postId === parent.id);
+      return data.reviews.filter(
+        (review: IReview) => review.postId === parent.id
+      );
     }
   },
   Review: {
@@ -75,7 +135,7 @@ const resolverMap = {
     post: (parent: IReview): IPost | undefined => {
       return data.posts.find((post: IPost) => post.id === parent.postId);
     }
-  },
-}
+  }
+};
 
 export default resolverMap;
--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,30 @@ import { createServer } from 'http';
 import compression from 'compression';
 import cors from 'cors';
 import schema from './schema';
+const models = require('../models');
 
 const app = express();
+
+// const server = new ApolloServer({
+//   schema,
+//   validationRules: [depthLimit(7)],
+//   context: ({ req }) => {
+//     console.log(req.headers.authorization);
+//   }
+// });
 
 const server = new ApolloServer({
   schema,
   validationRules: [depthLimit(7)],
+  context: async ({req}) => {
+    const token = req.headers.authorization;
+    let currentUser = null;
+    if (token !== undefined) {
+      const allusers = await models.User.findAll({ where: { token } });
+      currentUser = allusers[0].dataValues;
+    }
+    return { models, currentUser };
+  }
 });
 
 app.use('*', cors());
